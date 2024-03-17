fdfa1b2afefae944dcb44fae47591f70342315cc
Rudreshwar Baranwal <baranwalrudreshwar@gmail.com>
2024-03-17 07:55:17 +0530

    modified basic-react

--- basic-react/src/index.js
+++ basic-react/src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+/*
+This object can not be directly used in render function.
+As it is made by taking naming convention by our own.
+But React's render method has other props to take.
+
+const reactElement = {
+    type: 'a',
+    props: {
+        href: "https://google.com",
+        target: "_blank"
+    },
+    children: "Click to visit Google."
+}
+*/
+
+// This object will work in root.render as it is written according to React jsx.
+const anotherElement = (
+    <a href="https://google.com" target="_blank" rel="noreferrer">Click me to visit Google using normal JSX</a>
+)
+
+// Object created using React's React.createElement ... works perfectly
+const newElement = React.createElement (
+    'a',
+    {
+        href: "https://google.com",
+        target: "_blank"    
+    },
+    "Click me to visit Google. using React.createElement"
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        {anotherElement}
+        <br/>
+        {newElement}
+    </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
